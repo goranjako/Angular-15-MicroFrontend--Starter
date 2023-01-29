15e4f2ae0c01bd07b205257184f2cdb37428fbbd
goranjako <goranjako@gmail.com>
2023-01-29 21:38:40 +0100

    auth

--- Angular-15_MicroFrontend_eCommerce_Starter/projects/auth-lib/src/public-api.ts
+++ Angular-15_MicroFrontend_eCommerce_Starter/projects/auth-lib/src/public-api.ts
@@ -5,7 +5,5 @@
 export * from './lib/auth-lib.service';
 export * from './lib/auth-lib.component';
 export * from './lib/auth-lib.module';
-export * from './lib/auth/login/login.component';
-export * from './lib/auth/register/register.component';
 export * from './lib/auth/auth.guard';
 export * from './lib/auth/interceptor.interceptor';
